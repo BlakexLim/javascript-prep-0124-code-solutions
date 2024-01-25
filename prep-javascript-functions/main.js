244cb271cef5a87afbcc63ee6c200bf68a265b0a
Daradypo (Blake) Lim <blakelim007@gmail.com>
2024-01-25 22:09:00 +0000

    revised prep-javasrcript-functions exercise

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -1,5 +1,5 @@
 function sayHello(nickname) {
-  var greeting = 'Hello, ' + nickname + '!';
+  const greeting = 'Hello, ' + nickname + '!';
   console.log(greeting);
 }
 sayHello('Blake');
@@ -8,69 +8,66 @@ sayHello('Bryan');
 sayHello('Sal');
 
 function getFullName(firstName, lastName) {
-  var fullName = firstName + ' ' + lastName;
+  const fullName = firstName + ' ' + lastName;
   return fullName;
 
-  var greeting = 'Hello ' + fullName + '!';
-    return greeting;
+  const greeting = 'Hello ' + fullName + '!';
+  return greeting;
 }
 
-var blake = getFullName('Blake', 'Lim');
-console.log(blake);
+const daradypo = getFullName('Blake', 'Lim');
+console.log(daradypo);
 
-
-function addTwoNumbers (num1, num2) {
-  return num1 + num2
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
 }
-let twoAddedNumbers = addTwoNumbers (7, 11);
-console.log("addTwoNumbers exercise: " + twoAddedNumbers);
+const twoAddedNumbers = addTwoNumbers(7, 11);
+console.log('addTwoNumbers exercise: ' + twoAddedNumbers);
 
-
-function convertHoursToMinutes (hours) {
-  return hours * 60
+function convertHoursToMinutes(hours) {
+  return hours * 60;
 }
-let hour = convertHoursToMinutes(4);
-console.log("convertHoursToMinutes exercise:", hour);
-
+const hour = convertHoursToMinutes(4);
+console.log('convertHoursToMinutes exercise:', hour);
 
 function getGreeting(name) {
-  return "Hello " + name
+  return 'Hello ' + name;
 }
-let greeting = getGreeting('world');
+const greeting = getGreeting('world');
 console.log(greeting);
 
-function addAndMultiplyBy5 (num1, num2) {
-  return (num1 + num2) * 5
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
 }
-let product = addAndMultiplyBy5(5, 2);
+const product = addAndMultiplyBy5(5, 2);
 console.log(product);
 
 function multiplyAndDivideBy5(num1, num2) {
-  return (num1 * num2) / 5
+  return (num1 * num2) / 5;
 }
-let result = multiplyAndDivideBy5(5, 10);
+const result = multiplyAndDivideBy5(5, 10);
 console.log(result);
 
 function subtractTwoNumbers(num1, num2) {
-  return (num1 - num2)
+  return num1 - num2;
 }
-let subtract = subtractTwoNumbers(20, 15);
+const subtract = subtractTwoNumbers(20, 15);
 console.log(subtract);
 
 function getCircleCircumference(radius) {
-  return (2 * 3.14159) * radius
+  return 2 * 3.14159 * radius;
 }
-let circle = getCircleCircumference(3);
+const circle = getCircleCircumference(3);
 console.log(circle);
 
 function getFullName(firstName, lastName) {
-  return firstName + ' ' + lastName
+  return firstName + ' ' + lastName;
 }
-let fullName = getFullName('Blake', 'Lim');
+const fullName = getFullName('Blake', 'Lim');
 console.log(fullName);
 
 function cube(number) {
-  return number * number * number
+  return number * number * number;
 }
-let cubes = cube(3);
+const cubes = cube(3);
 console.log(cubes);
